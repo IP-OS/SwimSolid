afedd7976ad47b4612b22f3c499eae007948cc8d
KikiManjaro <kylian.meulin@gmail.com>
2023-06-13 22:09:13 +0200

    wip

--- contracts/Achievements.sol
+++ contracts/Achievements.sol
@@ -1,21 +1,34 @@
-// SPDX-License-Identifier: GPL-3.0
-
-pragma solidity ^0.5.9;
+pragma solidity ^0.8.20;
 
 import "@0x/contracts-erc20/contracts/src/ERC20Token.sol";
-
+import "Achievement.sol";
 
 contract Achievements is ERC20Token {
+    address owner;
+    address manager;
 
-    mapping public achievements;
-    mapping public usersAchievements;
+    mapping achievements(uint256 => Achivement) public;
+    mapping usersAchievements(uint256 => address) public;
 
     constructor () public {}
+
+    modifier isOwner {
+        require(msg.sender == owner);
+        _;
+    }
+
+    
+    modifier isManager {
+        require(msg.sender == manager);
+        _;
+    }
 
     createAchievement(name: string, description: string, reward: string)
 
     claimAchievement(id:uint256, user:address) isManager
 
-    addManager(_address: address) isOwner
+    setManager(_address: address) isOwner {
+        manager = _address
+    }
 
 }
--- contracts/Manager.sol
+++ contracts/Manager.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.9;
+pragma solidity ^0.8.20;
 
 import "@0x/contracts-erc20/contracts/src/ERC20Token.sol";
 
